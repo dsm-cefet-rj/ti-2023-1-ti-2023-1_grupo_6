08d748c61e784d790fdb7b1b1287b5644de18ccd
Isabele Rocha <isabeledpr@gmail.com>
2023-04-23 15:14:59 -0300

    barra de pesquisa incompleta

--- src/components/Pages/Header/index.js
+++ src/components/Pages/Header/index.js
@@ -1,44 +1,82 @@
-import './style.css';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { CarrinhoContext } from '../CarrinhoContext/CarrinhoContext.js';
+import { useContext } from 'react';
 import bag from '../../../assets/bag.svg';
 import blueIcon from '../../../assets/blueIcon.png';
-import { useNavigate } from 'react-router-dom';
-import  { useContext } from 'react';
-import { CarrinhoContext } from '../CarrinhoContext/CarrinhoContext.js';
+import './style.css';
 
 const MenuWeb = () => {
     const navigate = useNavigate();
     const { carrinho } = useContext(CarrinhoContext);
+    const lojas = useSelector((state) => state.lojas);
+    const [searchTerm, setSearchTerm] = useState('');
+    const lojasFiltradas = lojas.filter( (loja) => loja.nome.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 );
+
+    const handleSearch = (event) => {
+        const value = event.target.value;
+        setSearchTerm(value);
+    };
 
     return (
         <div className="app-menu-web">
-        <div className="icon-nav">
-            <img src={blueIcon} alt="logo" className="img-blueIcon"/>
-        </div>
+            <div className="icon-nav">
+            <   img src={blueIcon} alt="logo" className="img-blueIcon" />
+            </div>
 
-            <nav className = "menu-nav menu-home-profile">
-            <ul className = "nav-ul">
-                <li> <a href="#" onClick={() => {navigate("/home")}}>Início</a></li>
-                <li><a href="#" onClick={() => {navigate("/perfil")}}>Perfil</a></li>
-            </ul>
+            <nav className="menu-nav menu-home-profile">
+                <ul className="nav-ul">
+                    <li>
+                        <a href="#" onClick={() => navigate('/home')}>
+                        Início
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" onClick={() => navigate('/perfil')}>
+                        Perfil
+                        </a>
+                    </li>
+                </ul>
             </nav>
-        
-        <div className="search-nav nav-search-carrinho">
-            <input type="text" placeholder='Busque por item ou loja' className="input-search-nav" maxlength="100"/>
-        </div>
 
-        <nav className = "menu-nav nav-search-carrinho">
-            <ul className = "nav-ul">
-                <li>
-                    <a href="#" onClick={() => {navigate("/carrinho")}}>
-                    <p className="quant-itens-carrinho" style={{ fontSize: '15px' }}>{carrinho.quantidade}</p>
-                    <img src={bag} alt="carrinho" className="img-bag"/>
-                    </a>
-                </li>
-            </ul>
-            </nav>
+            <div className="search-nav nav-search-carrinho">
+                <input
+                type="text"
+                placeholder="Busque por loja"
+                className="input-search-nav"
+                maxLength="100"
+                value={searchTerm}
+                onChange={handleSearch}
+                />
+
+                {searchTerm && lojasFiltradas.length > 0 && (
+                <ul className="search-results" style={{ listStyle: 'none' }}>
+                    {lojasFiltradas.map((loja) => (
+                    <li key={loja.id}>
+                        <a href={`/home/lojas/servicos${loja.url}`}>
+                            {loja.nome} {loja.img}
+                        </a>
+                    </li>
+                    ))}
+                </ul>
+                )}
+
+            </div>
+
+            <nav className = "menu-nav nav-search-carrinho">
+                <ul className = "nav-ul">
+                    <li>
+                        <a href="#" onClick={() => {navigate("/carrinho")}}>
+                        <p className="quant-itens-carrinho" style={{ fontSize: '15px' }}>{carrinho.quantidade}</p>
+                        <img src={bag} alt="carrinho" className="img-bag"/>
+                        </a>
+                    </li>
+                </ul>
+                </nav>
 
         </div>  
-        );
+    );
 }
     
 export default MenuWeb;
