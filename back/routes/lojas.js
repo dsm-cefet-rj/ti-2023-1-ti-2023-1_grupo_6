1dd5b842529a8635def91544f825fd3f6d4d4bca
Gabriela <96033603+Gabrielasl18@users.noreply.github.com>
2023-06-19 23:47:30 -0300

    Update lojas.js

--- back/routes/lojas.js
+++ back/routes/lojas.js
@@ -1,64 +1,77 @@
 var express = require('express');
 var router = express.Router();
-const Shop = require('../models/estabelecimento');
+const Person = require('../models/usuario');
 
-// add loja
-router.post('/estabelecimento', async (req, res) => {
-    const { cnpj, senha, nome, animais_atendidos, contato, endereco, descricao, url } = req.body
-    const shop = {
-        cnpj,
-        senha,
-        informacoes:
-        {
-            nome,
-            animaisAtendidos,
-            endereco,
-            contato,
-            url,
-            descricao
-        }
-    }
+//get user
+router.get('/:id', async(req, res) => {
+    const id = req.params.id
+
     try {
-        await Shop.create(shop)
-        res.status(201).json({ message: 'Estabelecimento criado com sucesso!' })
-    }
-    catch (error) {
+        const person = await Person.findOne({_id: id})
+        if (!person) {
+            res.status(422).json({message: 'Usuário não encontrado!'})
+            return
+        }
+
+        res.status(200).json(person)
+    } catch (error) {
         res.status(500).json({ error: error })
     }
 });
 
 //delete user
-router.delete('/:id', async (req, res) => {
-    const id = req.params.id
-    const shop = await Shop.findOne({ _id: id })
-
-    if (!shop) {
-        res.status(422).json({ message: 'Usuário não encontrado!' })
-        return
-    }
-    try {
-        await Shop.deleteOne({ _id: id })
-        res.status(200).json({ message: 'Usuário deletado!' })
-    }
-    catch (error) {
-        res.status(500).json({ error: error })
-    }
-});
-
-//get user
-router.get('/:id', async (req, res) => {
+router.delete('/:id', async(req, res) => {
     const id = req.params.id
 
-    try {
-        const shop = await Shop.findOne({ _id: id })
-
-        if (!shop) {
-            res.status(422).json({ message: 'Usuário não encontrado!' })
+    const person = await Person.findOne({_id: id})
+        if (!person) {
+            res.status(422).json({message: 'Usuário não encontrado!'})
             return
         }
-        res.status(200).json(shop)
-    }
-    catch (error) {
+    try {
+
+        await Person.deleteOne({_id: id})
+        res.status(200).json({message: 'Usuário deletado!'})
+
+    } catch (error) {
         res.status(500).json({ error: error })
     }
 });
+// add user
+router.post('/usuario', async (req, res) =>{
+    const {email, senha, nome, cpf, dataNascimento} = req.body
+    const person ={
+        email,
+        senha,
+        nome,
+        cpf,
+        dataNascimento
+    }
+    try {
+
+        await Person.create(person)
+
+        res.status(201).json({message: 'Usuário criado com sucesso!'})
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+
+});
+
+router.route('/login')
+.post((req, res, next)=>{
+    const id = verificaLogin(req.body);
+    if(id){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(id);
+    }else{
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(id);
+    }
+})
+
+
+module.exports = router;
