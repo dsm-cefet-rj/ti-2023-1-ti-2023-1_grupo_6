dbf2b53b631e59f5e689a32288352540e0e25deb
caiquemorelli <52370192+caiquemorelli@users.noreply.github.com>
2023-06-20 02:25:02 -0300

    Update carrinho.js

--- back/routes/carrinho.js
+++ back/routes/carrinho.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const Carrinho = require('../models/carrinho');
 let carrinho = [
     {
         idUsuario: 1,
@@ -17,95 +17,100 @@ let carrinho = [
     }
 ];
 
-const { getProduto } = require('./produtos');
+const {getProduto} = require('./produtos');
 
-router.route("/:id")
-    .get((req, res, next) => {
-        const produtosUsuario = carrinho.filter(c => c.idUsuario == parseInt(req.params.id));
-        if (produtosUsuario.length > 0) {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(produtosUsuario[0].produto);
-        } else {
-            res.statusCode = 400;
-            res.setHeader('Content-Type', 'application/json');
-            res.json([]);
+router.get("/:id_usuario", async(req,res)=>{
+    const id_usuario = req.params.id
+
+    try {
+        const carrinhos = await Carrinho.findOne({_id: id_usuario})
+        if (!carrinhos) {
+            res.status(422).json({message: 'Usuário não encontrado!'})
+            return
         }
+        res.status(200).json(carrinhos)
+    } catch (error) {
+        res.status(500).json({ error: error })
     }
-    )
-router.route("/adicionar")
-    .post((req, res, next) => {
-        try {
-            const idProduto = req.body.idProduto;
-            const quantidade = req.body.quantidade;
-            const idUsuario = req.body.idUsuario;
-            let carrinhoUsuario = carrinho.filter(c => c.idUsuario === idUsuario);
-            if (carrinhoUsuario.length == 0) {
-                const produtoUsuario = getProduto(idProduto, quantidade);
-                carrinhoUsuario = {
-                    idUsuario: idUsuario,
-                    produto: [produtoUsuario]
-                };
-                carrinho.push(carrinhoUsuario);
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(carrinhoUsuario[0].produto);
-            } else {
-                let produto = carrinhoUsuario[0].produto.filter(p => p.id == idProduto);
-                if (produto.length > 0) {
-                    const indexCarrinho = carrinho.indexOf(carrinhoUsuario[0])
-                    const indexProduto = carrinhoUsuario[0].produto.indexOf(produto[0]);
-                    const qtd = produto[0].quantidade + quantidade;
-                    produto = getProduto(idProduto, qtd);
-                    carrinho[indexCarrinho].produto[indexProduto].quantidade = produto.quantidade;
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(carrinhoUsuario[0].produto);
-                } else {
-                    const produtoUsuario = getProduto(idProduto, quantidade);
-                    carrinhoUsuario[0].produto.push(produtoUsuario);
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(carrinhoUsuario[0].produto);
-                }
-            }
-        } catch (err) {
-            console.log(err)
-            res.statusCode = 400;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(false);
-        }
-    })
+});
 
-router.route("/")
-    .delete((req, res, next) => {
-        try {
-            const idProduto = req.body.idProduto;
-            const idUsuario = req.body.idUsuario;
-            let carrinhoUsuario = carrinho.filter(c => c.idUsuario === idUsuario);
-            let produto = carrinhoUsuario[0].produto.filter(p => p.id == idProduto);
-            const indexCarrinho = carrinho.indexOf(carrinhoUsuario[0])
-            const indexProduto = carrinhoUsuario[0].produto.indexOf(produto[0]);
-            const qtd = produto[0].quantidade - 1;
-            if (qtd > 0) {
-                console.log(1)
-                produto = getProduto(idProduto, qtd);
-                carrinho[indexCarrinho].produto[indexProduto].quantidade = produto.quantidade;
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(carrinhoUsuario[0].produto);
-            } else {
-                console.log(2)
-                carrinho[indexCarrinho].produto.splice(indexProduto, 1);
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(carrinhoUsuario[0].produto);
-            }
-        } catch {
-            res.statusCode = 400;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(false);
+router.route("/adicionar")
+  .post(async (req, res) => {
+    try {
+      const id_produto = req.body.id_produto;
+      const quantidade = req.body.quantidade;
+      const id_usuario = req.body.id_usuario;
+      let carrinhoUsuario = await Carrinho.find({ idUsuario: id_usuario });
+      if (carrinhoUsuario.length === 0) {
+        const produtoUsuario = await getProduto(id_produto, quantidade);
+        carrinhoUsuario = new Carrinho({
+          idUsuario: id_usuario,
+          produto: [produtoUsuario]
+        });
+        await carrinhoUsuario.save();
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(carrinhoUsuario.produto);
+      } else {
+        let produto = carrinhoUsuario[0].produto.filter(p => p.id == id_produto);
+        if (produto.length > 0) {
+          const indexProduto = carrinhoUsuario[0].produto.indexOf(produto[0]);
+          const qtd = produto[0].quantidade + quantidade;
+          produto = await getProduto(id_produto, qtd);
+          carrinhoUsuario[0].produto[indexProduto].quantidade = produto.quantidade;
+          await carrinhoUsuario[0].save();
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(carrinhoUsuario[0].produto);
+        } else {
+          const produtoUsuario = await getProduto(id_produto, quantidade);
+          carrinhoUsuario[0].produto.push(produtoUsuario);
+          await carrinhoUsuario[0].save();
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(carrinhoUsuario[0].produto);
         }
-    })
+      }
+    } catch (error) {
+      console.log(error);
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(false);
+    }
+  });
+
+  router.route("/")
+  .delete(async (req, res, next) => {
+      try {
+          const id_produto = req.body.idProduto;
+          const id_usuario = req.body.id_usuario;
+          let carrinhoUsuario = await Carrinho.find({ id_usuario: id_usuario });
+          let produto = carrinhoUsuario[0].produto.filter(p => p.id == id_produto);
+          const indexCarrinho = carrinho.indexOf(carrinhoUsuario[0]);
+          const indexProduto = carrinhoUsuario[0].produto.indexOf(produto[0]);
+          const qtd = produto[0].quantidade - 1;
+          if (qtd > 0) {
+              console.log(1);
+              produto = await getProduto(id_produto, qtd);
+              carrinhoUsuario[0].produto[indexProduto].quantidade = produto.quantidade;
+              await carrinhoUsuario[0].save();
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'application/json');
+              res.json(carrinhoUsuario[0].produto);
+          } else {
+              console.log(2);
+              carrinhoUsuario[0].produto.splice(indexProduto, 1);
+              await carrinhoUsuario[0].save();
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'application/json');
+              res.json(carrinhoUsuario[0].produto);
+          }
+      } catch (error) {
+          console.log(error);
+          res.statusCode = 400;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(false);
+      }
+  });
 
 module.exports = router;
