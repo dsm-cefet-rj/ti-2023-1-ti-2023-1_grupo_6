9e23ca518f2c97c752864db33a6c7a1c7a62742a
RrafaelR <augusto.ac.teixeira@gmail.com>
2023-06-21 00:18:12 -0300

    arrumada conexao com bd

--- back/models/usuario.js
+++ back/models/usuario.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose')
 
 // Define usuário
-const userInfoSchema = new mongoose.Schema({
+
+const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     nome: { type: String, required: true },
     dataNascimento: { type: Date, required: true },
     cpf: { type: String, required: true, unique: true },
-});
-
-const userSchema = new mongoose.Schema({
     senha:{ type: String, required: true },
-    informacoes: userInfoSchema,
 });
 
 const Person = mongoose.model('Person', userSchema);
